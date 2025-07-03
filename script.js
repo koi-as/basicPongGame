73fedd5e38f6750f596d8c43ba54a4ffc7e47ad0
koi-as <lightningblade.901@gmail.com>
2025-07-03 16:27:45 -0400

    Planning basic features and additional features to add to my pong game as comments in the script.js file. This will hopefully act as a guide through development.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('hi');
+
+// Mechanics of a pong game:
+// - Paddles
+//  - Move up and down
+//  - Ball bounces off paddles
+// - Ball
+//  - Bounces off everything, normally in a diagonal manner
+//  - Start of game, ball gets sent in a random direction
+//  - After a point is scored, send ball to non-scoring player
+// - Score
+//  - Increments up 1 each time ball passes edge of screen
+
+// What other game elements should I consider?
+// Start game/restart game buttons. This would be nice when playing with friends, so CHECK
+// I don't think a leaderboard would be necessary. Correct, leaderboard unnecessary, so NIX
+// No need to have a highscore setting. Correct, highscore unnecessary, so NIX
+// I could work on having interchangeable backgrounds, that would be a good addition to this project that could help me in other projects. This might be difficult, but I'll try, so CHECK
