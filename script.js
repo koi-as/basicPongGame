0a57f730a1118202f90203430305f9c5a645184c
koi-as <lightningblade.901@gmail.com>
2025-07-04 13:02:11 -0400

    Brainstormed and commented into script.js more detailed information about variables, functions, and features to develop.

--- script.js
+++ script.js
@@ -1,15 +1,71 @@
-console.log('hi');
-
 // Mechanics of a pong game:
 // - Paddles
-//  - Move up and down
-//  - Ball bounces off paddles
+//      - Move up and down
+//          - If keyUp = pressed {paddleVelocity = -1}; If keyDown = pressed {paddleVelocity = 1}
+//          - vars might be paddleVelocityL and paddleVelocityR for left and right players respectively
+//      - Ball bounces off paddles
+//          - If ballLocation = paddleLocation {velocityX * -1}
 // - Ball
-//  - Bounces off everything, normally in a diagonal manner
-//  - Start of game, ball gets sent in a random direction
+//      - Bounces off everything, normally in a diagonal manner
+//          - hits top/bottom wall -> velocityY invert
+//          - hits paddle -> velocityX invert
+//      - Start of game, ball gets sent in a random direction
+//          - velocityX and velocityY get randomly set to a value of 1 or -1
+//          - How to start game?
+//              - Start button?
+//              - Tap a movement key?
+//              - Timer countdown from 3 down to 1 then START and movement unlocks as ball gets launched
 //  - After a point is scored, send ball to non-scoring player
+//      - Ball moves out of bounds, add point to scorer (left paddle scores on right side; right paddle scores on left side)
+//          - Detect scorer (based on wall? if(ballLocation <= 0) {scoreR++} else if(ballLocation >= rightWallPosition) {scoreL++})
+//          - If scorer scores {velocityX = oppositeDirection; velocityY = randomValue of either 1/-1}
 // - Score
-//  - Increments up 1 each time ball passes edge of screen
+//      - Increments up 1 each time ball passes edge of screen
+//          - scorerL = ballLocation >= rightWallEdge {scoreL++; restart(scorerL)}
+//          - scorerR = ballLocation <= leftWallEdge {scoreR++; restart(scorerR)}
+//          - function restart(scorer) {detect scorer; set velocityX to opposite direction; reset paddleLocation; startGame()}
+//              - function name restart or reset?
+//      - Score is first to 7
+//          - If scoreL === 7 {endGame()}
+//          - If scoreR === 7 {endGame()}
+
+// left player vars
+/*
+let paddleLeft = "":
+let paddleLeftVelocity = 0;
+let playerLeft = "";
+let scoreLeft = 0;
+*/
+
+// right player vars
+/*
+let paddleRight = "";
+let paddleRightVelocity = "";
+let playerRight = "";
+let scoreRight = 0;
+*/
+
+// ball vars
+/*
+let ball = "";
+let ballVelocityX = 0;
+let ballVelocityY = 0;
+let ballLocation = 0;
+*/
+
+// functions
+/*
+playGame();
+endGame();
+resetGame();
+drawBall();
+drawPaddles();
+drawScore(); maybe combine this with a different function?
+*/
+
+
+
+
 
 // What other game elements should I consider?
 // Start game/restart game buttons. This would be nice when playing with friends, so CHECK
